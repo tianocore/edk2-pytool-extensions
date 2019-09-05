276ae4a586039f88c6f826cc7acca25a5f8f0793
matthewfcarlson <matthewfcarlson@users.noreply.github.com>
2019-09-05 11:57:34 -0700

    fix flake issue

--- edk2toolext/edk2_invocable.py
+++ edk2toolext/edk2_invocable.py
@@ -15,7 +15,6 @@ from edk2toolext.environment import shell_environment
 from edk2toollib.utility_functions import GetHostInfo
 from edk2toollib.utility_functions import locate_class_in_module
 from edk2toollib.utility_functions import import_module_by_file_name
-from edk2toollib.utility_functions import is_instance
 from edk2toolext.base_abstract_invocable import BaseAbstractInvocable
 
 
@@ -120,11 +119,11 @@ Key=value will get passed to build process for given build type)'''
                 # Filter through the Module, we're only looking for classes.
                 classList = [getattr(Module, obj) for obj in module_contents if inspect.isclass(getattr(Module, obj))]
                 # Get the name of all the classes
-                classNameList = [obj.__name__ for obj in classList if is_instance(obj)]
-                # TODO improve is_instance as it catches imports as well as declared classes
+                classNameList = [obj.__name__ for obj in classList]
+                # TODO improve filter to no longer catch imports as well as declared classes
                 imported_classes = ", ".join(classNameList)  # Join the classes together
                 print(f"The module you imported contains {imported_classes}")
-            except:
+            except Exception:
                 # Otherwise, oh well we'll just ignore this.
                 raise
                 pass
