c8b32ff012522e718c90d78139b7d74c5c648664
matthewfcarlson <matthewfcarlson@users.noreply.github.com>
2019-09-26 20:33:15 -0700

    added uefi builder test

--- edk2toolext/tests/test_uefi_build.py
+++ edk2toolext/tests/test_uefi_build.py
@@ -8,10 +8,16 @@
 ##
 import unittest
 from edk2toolext.environment import uefi_build
+from edk2toolext.environment.plugintypes import uefi_helper_plugin
+from edk2toolext.environment.plugin_manager import PluginManager
+import argparse
+import tempfile
 
 
 class TestUefiBuild(unittest.TestCase):
+
     def setUp(self):
+        self.WORKSPACE = tempfile.mkdtemp()
         pass
 
     def tearDown(self):
@@ -28,6 +34,36 @@ class TestUefiBuild(unittest.TestCase):
     def test_init(self):
         builder = uefi_build.UefiBuilder()
         self.assertIsNotNone(builder)
+
+    def test_commandline_options(self):
+        builder = uefi_build.UefiBuilder()
+        parserObj = argparse.ArgumentParser()
+        builder.AddCommandLineOptions(parserObj)
+        args = [
+            ["--CLEAN", "--SKIPBUILD"],
+            ["--FLASHONLY"],
+            ["--CLEANONLY"],
+            ["--FLASHROM"],
+            ["--UPDATECONF"],
+            ["--FLASHONLY"],
+            ["--SKIPPREBUILD"],
+            ["--SKIPPOSTBUILD"]
+        ]
+        for argpart in args:
+            results = parserObj.parse_args(argpart)
+            builder.RetrieveCommandLineOptions(results)
+
+    def test_go_skip_building(self):
+        builder = uefi_build.UefiBuilder()
+        builder.SkipPostBuild = True
+        builder.SkipBuild = True
+        builder.SkipBuild = True
+        manager = PluginManager()
+        helper = uefi_helper_plugin.HelperFunctions()
+        ret = builder.Go(self.WORKSPACE, "", helper, manager)
+        self.assertEqual(ret, 0)
+
+    # TODO finish unit test
 
 
 if __name__ == '__main__':
