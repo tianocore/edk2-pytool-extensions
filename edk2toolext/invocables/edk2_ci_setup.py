3c8d9173107376d73249b124e4e903866f46c7ac
Sean Brogan <spbrogan@live.com>
2019-10-10 20:22:45 -0700

    Fix omnicache path check for None and remove from Settings

--- edk2toolext/invocables/edk2_ci_setup.py
+++ edk2toolext/invocables/edk2_ci_setup.py
@@ -37,10 +37,6 @@ class CiSetupSettingsManager(MultiPkgAwareSettingsInterface):
         ''' get WorkspacePath '''
         raise NotImplementedError()
 
-    def GetOmnicachePath(self):
-        ''' Optionally point to omnicache path '''
-        pass
-
     def AddCommandLineOptions(self, parserObj):
         ''' Implement in subclass to add command line options to the argparser '''
         pass
@@ -75,9 +71,12 @@ class Edk2CiBuildSetup(Edk2MultiPkgAwareInvocable):
     def RetrieveCommandLineOptions(self, args):
         '''  Retrieve command line options from the argparser '''
         self.git_ignore = args.git_ignore
-        self.omnicache_path = args.omnicache_path
         self.git_force = args.git_force
         self.git_update = args.git_update
+        self.omnicache_path = args.omnicache_path
+        if (self.omnicache_path is not None) and  (not os.path.exists(self.omnicache_path)):
+            logging.warning(f"Omnicache path set to invalid path: {args.omnicache_Path}")
+            self.omnicache_path = None
 
         super().RetrieveCommandLineOptions(args)
 
@@ -92,18 +91,11 @@ class Edk2CiBuildSetup(Edk2MultiPkgAwareInvocable):
         return "CISETUP"
 
     def Go(self):
-        # Parse command line arguments
-
-        omnicache_path = self.omnicache_path
-        try:
-            omnicache_path = self.PlatformSettings.GetOmnicachePath()
-        except:
-            pass
 
         ret = repo_resolver.resolve_all(self.GetWorkspaceRoot(),
                                         self.PlatformSettings.GetDependencies(),
                                         ignore=self.git_ignore, force=self.git_force,
-                                        update_ok=self.git_update, omnicache_dir=omnicache_path)
+                                        update_ok=self.git_update, omnicache_dir=self.omnicache_path)
 
         logging.info(f"Repo resolver resolved {ret}")
 
