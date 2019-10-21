0e5acff62b5177505f8249cbcf49793ff60bbe29
Bret Barkelew <brbarkel@microsoft.com>
2019-10-21 15:45:35 -0700

    Add required pip modules for further development.

---
+++ edk2toolext/signer_plaything.py
@@ -0,0 +1,8 @@
+def signing_callback(data_to_sign, signature_options, signer_options):
+    # Signature Options:
+    # - Algorithm
+    # - Size
+
+    # Signer Options:
+    # - Key
+    # - All autosign options.
