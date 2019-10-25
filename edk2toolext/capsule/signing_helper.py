8a44dc8beceed78e46ceb5150b55e04d107431cd
Bret Barkelew <brbarkel@microsoft.com>
2019-10-25 10:49:48 -0700

    Enable full signing_helper module to load pypath modules and file modules.

--- edk2toolext/capsule/signing_helper.py
+++ edk2toolext/capsule/signing_helper.py
@@ -1,21 +1,22 @@
+import importlib
+
 from edk2toolext.capsule import pyopenssl_signer
 from edk2toolext.capsule import signtool_signer
+from edk2toollib.utility_functions import import_module_by_file_name
 
 PYOPENSSL_SIGNER = 'pyopenssl'
 SIGNTOOL_SIGNER = 'signtool'
 PYPATH_MODULE_SIGNER = 'pymodule'
 LOCAL_MODULE_SIGNER = 'local_module'
 
-def get_signer(type):
+def get_signer(type, specifier=None):
   if type == PYOPENSSL_SIGNER:
     return pyopenssl_signer
   elif type == SIGNTOOL_SIGNER:
     return signtool_signer
   elif type == PYPATH_MODULE_SIGNER:
-    # TODO: Import thing from the pypath.
-    pass
+    return importlib.import_module(specifier)
   elif type == LOCAL_MODULE_SIGNER:
-    # TODO: Import file.
-    pass
+    return import_module_by_file_name(specifier)
   else:
     return None
--- edk2toolext/capsule/tests/signing_helper_test.py
+++ edk2toolext/capsule/tests/signing_helper_test.py
@@ -6,6 +6,26 @@ import logging
 from edk2toolext.capsule import signing_helper
 
 class SignerLocationTests(unittest.TestCase):
-    @pytest.mark.skip(reason="test is incomplete")
-    def test_should_be_able_to_locate_builtin_modules(self):
-        pass
+    def test_should_be_able_to_fetch_a_builtin_signer_module(self):
+        pysigner = signing_helper.get_signer(signing_helper.PYOPENSSL_SIGNER)
+        self.assertTrue(hasattr(pysigner, 'sign'))
+
+        signtoolsigner = signing_helper.get_signer(signing_helper.SIGNTOOL_SIGNER)
+        self.assertTrue(hasattr(signtoolsigner, 'sign'))
+
+    def test_should_be_able_to_pass_a_signing_module(self):
+        pysigner = signing_helper.get_signer(signing_helper.PYPATH_MODULE_SIGNER, 'edk2toolext.capsule.pyopenssl_signer')
+        self.assertTrue(hasattr(pysigner, 'sign'))
+
+    def test_should_be_able_to_fetch_a_user_provided_signer_module(self):
+        test_script_path = os.path.dirname(os.path.abspath(__file__))
+        pysigner_path = os.path.join(os.path.dirname(test_script_path), 'pyopenssl_signer.py')
+        self.assertTrue(os.path.isfile(pysigner_path))
+        pysigner = signing_helper.get_signer(signing_helper.LOCAL_MODULE_SIGNER, pysigner_path)
+        self.assertTrue(hasattr(pysigner, 'sign'))
+
+# NOTE: These tests may not run on non-Windows or without the WDK installed.
+# class SigntoolSignerModuleTest(unittest.TestCase):
+#     def test_module_should_be_able_to_locate_signtool(self):
+#         signtoolsigner = signing_helper.get_signer(signing_helper.SIGNTOOL_SIGNER)
+#         self.assertTrue(os.path.isfile(signtoolsigner.get_signtool_path()))
