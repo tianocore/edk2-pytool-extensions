6cd4a5887022a061071f7c43efab18d294d74709
Bret Barkelew <brbarkel@microsoft.com>
2019-10-24 19:43:26 -0700

    Drop unnecessary file.

--- edk2toolext/signer_plaything.py
+++
@@ -1,8 +0,0 @@
-def signing_callback(data_to_sign, signature_options, signer_options):
-    # Signature Options:
-    # - Algorithm
-    # - Size
-
-    # Signer Options:
-    # - Key
-    # - All autosign options.
