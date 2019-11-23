c81f4e2b80b3ef23adcc44c1cbb3d19a1ae0d2e0
Jeremiah Cox <17728431+out0xb2@users.noreply.github.com>
2019-11-22 20:15:54 -0800

    UefiBuild must share the environment settings between parsing Dsc and Fdf.  DSC may define macros needed in FDF parsing (#121)

--- edk2toolext/environment/uefi_build.py
+++ edk2toolext/environment/uefi_build.py
@@ -476,7 +476,6 @@ class UefiBuilder(object):
     # Parse the Active platform DSC file.  This will get lots of variable info to
     # be used in the build.  This makes it so we don't have to define things twice
     #
-
     def ParseDscFile(self):
         if self.env.GetValue("ACTIVE_PLATFORM") is None:
             logging.error("The DSC file was not set. Please set ACTIVE_PLATFORM")
@@ -493,7 +492,6 @@ class UefiBuilder(object):
             for key, value in dscp.LocalVars.items():
                 # set env as overrideable
                 self.env.SetValue(key, value, "From Platform DSC File", True)
-
         else:
             logging.error("Failed to find DSC file")
             return -1
@@ -513,8 +511,14 @@ class UefiBuilder(object):
         if(os.path.isfile(self.mws.join(self.ws, self.env.GetValue("FLASH_DEFINITION")))):
             # parse the FDF file- fdf files have similar syntax to DSC and therefore parser works for both.
             logging.debug("Parse Active Flash Definition (FDF) file")
+
+            # Get the vars from the environment that are not build keys
+            input_vars = self.env.GetAllNonBuildKeyValues()
+            # Update with special environment set build keys
+            input_vars.update(self.env.GetAllBuildKeyValues())
+
             fdf_parser = DscParser().SetBaseAbsPath(self.ws).SetPackagePaths(
-                self.pp.split(os.pathsep)).SetInputVars(self.env.GetAllBuildKeyValues())
+                self.pp.split(os.pathsep)).SetInputVars(input_vars)
             pa = self.mws.join(self.ws, self.env.GetValue("FLASH_DEFINITION"))
             fdf_parser.ParseFile(pa)
             for key, value in fdf_parser.LocalVars.items():
