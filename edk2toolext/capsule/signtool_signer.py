dcee154b5706a5a8aaeeca5af4498508f2ddea62
Jeremiah Cox <17728431+out0xb2@users.noreply.github.com>
2020-04-04 09:24:14 -0700

    Add support for _embedded_ PKCS7 signing to signtool_signer sign() To the sign() function: * added type hints * In parameter dict "signature_options", the key "sign_alg" is deprecated and replaced with "format".  Its prior value "pkcs12" was NOT a signature format, it was the format of the signer's key file. Moving it to a new signer_option key "key_file_format" * New "signature_option" key "format" supports 1 value of "pkcs7" which is the true signature format being produced * Added "format_options" value to key "signature_options". Values "embedded" and "detachedSignatureData" are added to distinguish the different PKCS7 flavors supported.  Note that these format_options are mutually exclusive * To ensure compatibility with existing code that passes "signature_options" with key "pkcs12", sign() detects this and fills the new parameters with values the provide backwards compatible behavior

--- edk2toolext/capsule/signtool_signer.py
+++ edk2toolext/capsule/signtool_signer.py
@@ -20,6 +20,10 @@ from edk2toollib.windows import locate_tools
 from edk2toollib.utility_functions import RunCmd
 
 GLOBAL_SIGNTOOL_PATH = None
+SUPPORTED_SIGNTOOL_FORMAT_OPTIONS = {
+    'detachedSignedData',
+    'embedded'
+}
 
 
 def get_signtool_path():
@@ -36,18 +40,40 @@ def get_signtool_path():
     return GLOBAL_SIGNTOOL_PATH
 
 
-def sign(data, signature_options, signer_options):
+def sign(data: bytes, signature_options: dict, signer_options: dict) -> bytes:
     '''
     primary signing interface. Takes n the signature_options and signer_options
     dictionaries that are used by capsule_tool and capsule_helper
     '''
-    # NOTE: Currently, we only support the necessary algorithms for capsules.
-    if signature_options['sign_alg'] != 'pkcs12':
-        raise ValueError(f"Unsupported signature algorithm: {signature_options['sign_alg']}!")
+
+    # NOTE: Currently, we only support the necessary options for capsules & Windows Firmware Policies
+
+    # The following _if_ clause handles the deprecated signature_option 'sign_alg' for backwards compatibility
+    # when the deprecated option is supplied, this code adds the new, required options based on prior code behavior
+    if 'sign_alg' in signature_options:
+        print('!!! WARNING: signature_option "sign_alg" is deprecated, use "format" instead')
+        if signature_options['sign_alg'] == 'pkcs12':
+            print('assuming "signature_option" "format" of "pkcs7" with "format_options" of "detached"')
+            signature_options['format'] = 'pkcs7'
+            signature_options['format_options'] = 'DetachedSignedData'
+            print('assuming "signer_option" "key_file_format" of "pkcs12"')
+            signer_options['key_file_format'] = 'pkcs12'
+        else:
+            raise ValueError(f"Unsupported signature algorithm: {signature_options['sign_alg']}!")
+    if signature_options['format'] != 'pkcs7':
+        raise ValueError(f"Unsupported signature format: {signature_options['format']}!")
+    for opt in signature_options['format_options']:
+        if opt not in SUPPORTED_SIGNTOOL_FORMAT_OPTIONS:
+            raise ValueError(f"Unsupported format option: {opt}!  Ensure you have provied a set")
+    if 'embedded' in signature_options['format_options']:
+        if 'detachedSignedData' in signature_options['format_options']:
+            raise ValueError(f"format_options 'detachedSignedData' and 'embedded' are mutually exclusive")
     if signature_options['hash_alg'] != 'sha256':
         raise ValueError(f"Unsupported hashing algorithm: {signature_options['hash_alg']}!")
     if 'key_file' not in signer_options:
         raise ValueError("Must supply a key_file in signer_options for Signtool!")
+    if signer_options['key_file_format'] != 'pkcs12':
+        raise ValueError(f"Unsupported key file format: {signer_options['key_file_format']}!")
 
     # Set up a temp directory to hold input and output files.
     temp_folder = tempfile.mkdtemp()
@@ -61,7 +87,11 @@ def sign(data, signature_options, signer_options):
     # Start building the parameters for the call.
     signtool_params = ['sign']
     signtool_params += ['/fd', signature_options['hash_alg']]
-    signtool_params += ['/p7ce', 'DetachedSignedData']
+    if 'format_options' in signature_options:
+        if 'detachedSignedData' in signature_options['format_options']:
+            signtool_params += ['/p7ce', 'DetachedSignedData']
+        else:
+            signtool_params += ['/p7ce', 'Embedded']
     signtool_params += ['/p7', f'"{temp_folder}"']
     signtool_params += ['/f', f"\"{signer_options['key_file']}\""]
     if 'oid' in signer_options:
