f4c2c74f45ab11acc1a46bd62e55b14ab973b13c
Matthew Carlson <macarl@microsoft.com>
2020-05-21 12:35:52 -0700

    added a nuget package with an ext_dep inside

---
+++ edk2toolext/tests/minimal_uefi_tree/uefi_tree.py
@@ -0,0 +1,94 @@
+# this is for creating a minimal uefi tree for testing
+##
+# Copyright (c) Microsoft Corporation
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+
+import os
+import tempfile
+import shutil
+
+
+class uefi_tree:
+    ''' 
+    This class represents a minimal UEFI tree that you can setup
+    It is configurable and offers options to modify the tree for
+    '''
+
+    def __init__(self, workspace=None):
+        ''' copy the minimal tree to a folder, if one is not provided we will create a new temporary folder for you '''
+        if workspace == None:
+            workspace = os.path.abspath(tempfile.mkdtemp())
+        uefi_tree._copytree(uefi_tree._get_src_folder(), workspace, ignore=uefi_tree._copyfilter)
+        self.workspace = workspace
+
+    @staticmethod
+    def _get_src_folder():
+        return os.path.dirname(os.path.abspath(__file__))
+
+    @staticmethod
+    def _get_optional_folder():
+        return os.path.join(uefi_tree._get_src_folder(), "optional")
+
+    @staticmethod
+    def _copyfilter(src, names):
+        ''' make sure we don't copy anything we don't want '''
+        names = [x for x in names if x != "__pycache__"]
+        names = [x for x in names if x != os.path.basename(__file__)] # don't copy ourselves
+        names = [x for x in names if x != "optional"] # don't copy 
+        return names
+
+    @staticmethod
+    def _copytree(src, dst, symlinks=False, ignore=None):
+        # https://stackoverflow.com/questions/1868714/how-do-i-copy-an-entire-directory-of-files-into-an-existing-directory-using-pyth
+        items = os.listdir(src)
+        if ignore is not None:
+            items = ignore(src, items)
+        for item in items:
+            s = os.path.join(src, item)
+            d = os.path.join(dst, item)
+            if os.path.isdir(s):
+                shutil.copytree(s, d, symlinks, ignore)
+            else:
+                shutil.copy2(s, d)
+
+    @staticmethod
+    def write_to_file(path, contents, close=True):
+        f = open(path, "w")
+        f.writelines(contents)
+        if close:
+            f.close()
+
+    def get_settings_provider_path(self):
+        ''' gets the settings provider in the workspace '''
+        return os.path.join(self.workspace, "settings.py")
+
+    def get_optional_file(self, file):
+        ''' gets the path of an optional file '''
+        optional_path = os.path.join(uefi_tree._get_optional_folder(), file)
+        if os.path.exists(optional_path):
+            return os.path.abspath(optional_path)
+        raise ValueError(f"Optional file not found {file}")
+
+    def create_ext_dep(self, dep_type, name, version, source=None, scope="global", dir_path=""):
+        ''' creates an ext dep in your workspace '''
+        dep_type = dep_type.lower()
+        if source == None and dep_type == "nuget":
+            source = "https://api.nuget.org/v3/index.json"
+        if source == None:
+            raise ValueError("Source was not provided")
+        text = f'''
+        {{
+            "scope": "{scope}",
+            "type": "{dep_type}",
+            "name": "{name}",
+            "version": "{version}",
+            "source": "{source}",
+            "flags": []        
+        }}'''
+        ext_dep_name = name.replace(" ", "_")
+        file_name = f"{ext_dep_name}_ext_dep.json"
+        output_dir = os.path.join(self.workspace, dir_path)
+        os.makedirs(output_dir, exist_ok=True)
+        output_path = os.path.join(output_dir, file_name)
+        uefi_tree.write_to_file(output_path, text)
--- edk2toolext/tests/test_edk2_update.py
+++ edk2toolext/tests/test_edk2_update.py
@@ -16,36 +16,8 @@ from edk2toollib.utility_functions import RunCmd
 from edk2toolext.environment import shell_environment
 from edk2toolext.edk2_git import Repo
 from edk2toolext.environment import self_describing_environment
+from edk2toolext.tests.minimal_uefi_tree.uefi_tree import uefi_tree
 
-class_info = '''
-from edk2toolext.invocables.edk2_update import UpdateSettingsManager
-import os
-class TestSettingsManager(UpdateSettingsManager):
-
-    def GetActiveScopes(self):
-        return []
-
-    def GetWorkspaceRoot(self):
-        return os.path.dirname(__file__)  # assume the workspace is in the parent directory
-
-    def AddCommandLineOptions(self, parserObj):
-        pass
-
-    def RetrieveCommandLineOptions(self, args):
-        pass
-
-    def GetName(self):
-        return "TestUpdate"
-
-    def GetArchitecturesSupported(self):
-        return []
-
-    def GetPackagesSupported(self):
-        return []
-
-    def GetTargetsSupported(self):
-        return []
-'''
 
 class TestEdk2Update(unittest.TestCase):
 
@@ -57,7 +29,7 @@ class TestEdk2Update(unittest.TestCase):
         shell_environment.GetEnvironment().restore_initial_checkpoint()
         for temp_folder in TestEdk2Update.temp_folders:
             logging.info(f"Cleaning up {temp_folder}")
-            #shutil.rmtree(os.path.abspath(temp_folder), ignore_errors=True)
+            # shutil.rmtree(os.path.abspath(temp_folder), ignore_errors=True)
         TestEdk2Update.restart_logging()
         pass
 
@@ -80,34 +52,7 @@ class TestEdk2Update(unittest.TestCase):
         TestEdk2Update.temp_folders.append(temp_folder)
         return os.path.abspath(temp_folder)
 
-    @classmethod
-    def write_to_file(cls, path, contents, close=True):
-        f = open(path, "w")
-        f.writelines(contents)
-        if close:
-            f.close()
-
-    def create_local_git_repo(self, add_ignore=True):
-        ''' creates a git repo and gives it to you '''
-        git_dir = self.get_temp_folder()
-        ret = RunCmd("git", "init", workingdir=git_dir)
-        self.assertEqual(int(ret), 0, "We should not fail the git init")
-        repo = Repo(git_dir)
-        if add_ignore:
-            TestEdk2Update.write_to_file(os.path.join(git_dir, ".gitignore"), "*_extdep/")
-            self.commit_local_git_repo(repo)
-        return repo
-
-    def commit_local_git_repo(self, repo: Repo):
-        ''' checks in a commit to a local git repo '''
-        git_dir = repo.get_path()
-        ret = RunCmd("git", "add .", workingdir=git_dir)
-        self.assertEqual(int(ret), 0, "We should not fail the git add")
-        ret = RunCmd("git", "commit -a -m TEST", workingdir=git_dir)
-        self.assertEqual(int(ret), 0, "We should not fail the git commit")
-        return repo._get_head().commit
-
-    def invoke_update(self, settings_filepath, args = [], failure_expected=False):
+    def invoke_update(self, settings_filepath, args=[], failure_expected=False):
         builder = Edk2Update()
         sys.argv = ["stuart_update", "-c", settings_filepath]
         sys.argv.extend(args)
@@ -126,50 +71,17 @@ class TestEdk2Update(unittest.TestCase):
         builder = Edk2Update()
         self.assertIsNotNone(builder)
 
-
     def test_one_level_recursive(self):
+        ''' makes sure we can do a recursive update '''
         WORKSPACE = self.get_temp_folder()
+        tree = uefi_tree(WORKSPACE)
         logging.getLogger().setLevel(logging.WARNING)
-        settings_filepath = os.path.join(WORKSPACE, "settings.py")
-        TestEdk2Update.write_to_file(settings_filepath, class_info)
-        git_repo = self.create_local_git_repo()
-        self.assertNotEqual(git_repo.get_path(), None)
-        octocat_ext_dep = f'''
-        {{
-        "scope": "global",
-        "type": "git",
-        "name": "Octocat",
-        "version": "d0dd1f61b33d64e29d8bc1372a94ef6a2fee76a9",
-        "source": "https://github.com/octocat/Spoon-Knife.git",
-        "flags": []
-        }}
-        '''
-        TestEdk2Update.write_to_file(os.path.join(git_repo.get_path(), "octocat_ext_dep.json"), octocat_ext_dep)
-        git_hash = self.commit_local_git_repo(git_repo)
-
-        escaped_git_repo = git_repo.get_path().replace("\\","\\\\")
-        print(git_hash)
-        self.assertGreater(len(git_hash), 30)
-        local_ext_dep = f'''
-        {{
-        "scope": "global",
-        "type": "git",
-        "name": "MYREPO",
-        "version": "{git_hash}",
-        "source": "{escaped_git_repo}",
-        "flags": []
-        }}
-        '''
-        # now add the ext_dep to the git repo
-        TestEdk2Update.write_to_file(os.path.join(WORKSPACE, "myrepo_ext_dep.json"), local_ext_dep)
-
+        tree.create_ext_dep("nuget", "Edk2TestUpdate", "0.0.1")
         # Do the update
-        self.invoke_update(settings_filepath)
-
-        # now we should check that our workspace is how we expect it
-        (build_env, shell_env) = self_describing_environment.BootstrapEnvironment(WORKSPACE, [])
-        # make sure we find more than 1 path
-        self.assertNotEqual(build_env.extdeps, None)
-        self.assertGreater(len(build_env.extdeps), 0)
-        print(build_env.extdeps)
-        self.assertTrue(os.path.exists(WORKSPACE, "MYREPO_extdep", "MYREPO", "Octocat_extdep", "Octocat"))
+        self.invoke_update(tree.get_settings_provider_path())
+        # make sure it worked
+        # we're going to request two packages from nuget -> the first contains an extdep for the second
+        # Edk2TestUpdate_extdep
+        # Edk2TestUpdate_extdep\NuGet.CommandLine_extdep
+        self.assertTrue(os.path.exists(os.path.join(WORKSPACE, "Edk2TestUpdate_extdep",
+                                                    "NuGet.CommandLine_extdep", "extdep_state.json")))
