30fe3f489620f2de27053e70131024d7fef4be35
Matthew Carlson <macarl@microsoft.com>
2020-06-05 13:36:23 -0700

    added more tests

--- edk2toolext/tests/test_docgen.py
+++ edk2toolext/tests/test_docgen.py
@@ -11,6 +11,14 @@ import tempfile
 from edk2toolext.docgen import generate
 import sys
 
+default_config = {
+    "site_name": "edk2-pytool-extensions",
+    "repo_url": "https://github.com/tianocore/edk2-pytool-extensions",
+    "copyright": "SPDX-License-Identifier: BSD-2-Clause-Patent",
+    "site_description": "Python tools supporting UEFI EDK2 firmware development",
+    "site_url": "https://tianocore.github.io/edk2-pytool-extensions"
+}
+
 
 class Test_DocGenerator(unittest.TestCase):
 
@@ -19,13 +27,6 @@ class Test_DocGenerator(unittest.TestCase):
         self.assertIsNotNone(docgen)
 
     def test_can_setup_generator(self):
-        default_config = {
-            "site_name": "edk2-pytool-extensions",
-            "repo_url": "https://github.com/tianocore/edk2-pytool-extensions",
-            "copyright": "SPDX-License-Identifier: BSD-2-Clause-Patent",
-            "site_description": "Python tools supporting UEFI EDK2 firmware development",
-            "site_url": "https://tianocore.github.io/edk2-pytool-extensions"
-        }
         docgen = generate.DocGenerator()
         workspace = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
         docgen.set_workspace(workspace)
@@ -56,3 +57,41 @@ class Test_DocGenerator(unittest.TestCase):
             markdown_file.close()
         test_markdown = generate.DocGenerator.ConvertToFriendlyName(test_markdown_path)
         self.assertEqual(test_markdown, "Test Two")
+
+    def test_needs_default_config(self):
+        docgen = generate.DocGenerator()
+        workspace = tempfile.mkdtemp()
+        docgen.set_workspace(workspace)
+        docgen.set_should_include_test_documentation(False)
+        docgen.set_target_module("edk2toolext")
+
+        try:
+            docgen.generate()
+            self.fail()
+        except ValueError:
+            pass
+
+    def test_generate_documentation(self):
+        docgen = generate.DocGenerator()
+        workspace = tempfile.mkdtemp()
+        docgen.set_workspace(workspace)
+        docgen.set_config_defaults(**default_config)
+        docgen.set_target_module("edk2toolext", default_config["site_name"])
+        # make sure out output docs make sense
+        rel_path = os.path.relpath(docgen.get_output_docs_dir(), workspace)
+        self.assertNotIn("..", rel_path)
+        # make sure we don't generate html
+        docgen.set_hook(docgen.Hooks.PreHtml, lambda: sys.exit(0))
+        try:
+            docgen.generate()
+            self.fail()
+        except SystemExit:
+            # TODO: check that ret code is 0
+            pass
+        config = docgen.dump_config()
+        self.assertIn("nav", config)
+        print(len(config["nav"]))
+        self.assertGreater(len(config["nav"]), 1)
+
+test = Test_DocGenerator()
+test.test_generate_documentation()
