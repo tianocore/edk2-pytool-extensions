c30063bffc0746c78265c20e3bc04746db8efd6e
Sean Brogan <sean.brogan@microsoft.com>
2020-06-12 12:41:15 -0700

    wip

--- edk2toolext/invocables/edk2_pr_eval.py
+++ edk2toolext/invocables/edk2_pr_eval.py
@@ -77,7 +77,12 @@ class Edk2PrEval(Edk2MultiPkgAwareInvocable):
         return "PREVALLOG"
 
     def Go(self):
-        self.edk2_path_obj = path_utilities.Edk2Path(self.GetWorkspaceRoot(), self.GetPackagesPath())
+
+        # create path obj for resolving paths.  Since PR eval is run early to determine if a build is impacted by the changes
+        # of a PR we must ignore any packages path that are not valid due to not having their submodule or folder populated.
+        # A packages path is ok to drop for this because if it isn't populated it is assumed outside the repository and
+        # thus will not trigger the build.
+        self.edk2_path_obj = path_utilities.Edk2Path(self.GetWorkspaceRoot(), self.GetPackagesPath(), error_on_invalid_pp=False)
         self.logger = logging.getLogger("edk2_pr_eval")
         actualPackagesDict = self.get_packages_to_build(self.requested_package_list)
 
