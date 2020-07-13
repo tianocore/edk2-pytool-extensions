1892076558d97269ec0be798cafd1a0501428b39
Garand Tyson <t-gatys@microsoft.com>
2020-07-13 17:57:17 -0500

    Added code gerneration

--- edk2toolext/SBAT/PEObject.py
+++ edk2toolext/SBAT/PEObject.py
@@ -73,6 +73,7 @@ FILE_DATE_MS_STR         = "FileDateMS"
 FILE_DATE_LS_STR         = "FileDateLS"
 FILE_DATE_STR            = "FileDate"
 VFT_FONT_STR             = "VFT_FONT"
+TRANSLATION_STR          = "Translation"
 RSRC_STR                 = ".rsrc"
 STRING_FILE_INFO_STR     = "StringFileInfo"
 VAR_FILE_INFO_STR        = "VarFileInfo"
@@ -81,6 +82,10 @@ VAR_FILE_INFO_STR        = "VarFileInfo"
 PE_ENCODING              = "utf-8"
 PE_STRUCT_STR            = "Structure"
 PE_VALUE_STR             = "Value"
+BEGIN_STR                = "BEGIN"
+END_STR                  = "END"
+BLOCK_STR                = "BLOCK"
+VALUE_STR                = "VALUE"
 
 # Validation requirements
 VERSIONFILE_REQUIRED_FIELDS = {
@@ -119,6 +124,21 @@ STRING_FILE_INFO_REQUIRED_FIELDS = {
     COMPANY_NAME_STR,
     PRODUCT_NAME_STR,
     FILE_VERSION_STR
+}
+
+STRING_FILE_INFO_ALLOWED_FIELDS = {
+    "Comments",
+    "CompanyName",
+    "FileDescription",
+    "FileVersion",
+    "InternalName",
+    "LegalCopyright",
+    "LegalTrademarks",
+    "OriginalFilename",
+    "PrivateBuild",
+    "ProductName",
+    "ProductVersion",
+    "SpecialBuild"
 }
 
 VALID_FILE_OS_VALUES = {
@@ -164,6 +184,41 @@ VALID_SUBTYPE_VFT_FONT = {
     "VFT2_FONT_TRUETYPE"
 }
 
+VALID_LANG_ID = {
+    0x0401, 0x0402,
+    0x0403, 0x0404,
+    0x0405, 0x0406,
+    0x0407, 0x0408,
+    0x0409, 0x040A,
+    0x040B, 0x040C,
+    0x040D, 0x040E,
+    0x040F, 0x0410,
+    0x0411, 0x0412,
+    0x0413, 0x0414,
+    0x0415, 0x0416,
+    0x0417, 0x0418,
+    0x0419, 0x041A,
+    0x041B, 0x041C,
+    0x041D, 0x041E,
+    0x041F, 0x0420,
+    0x0421, 0x0804,
+    0x0807, 0x0809,
+    0x080A, 0x080C,
+    0x0C0C, 0x100C,
+    0x0816, 0x081A,
+    0x0810, 0x0813,
+    0x0814
+}
+
+VALID_CHARSET_ID = {
+    0x0000, 0x03A4,
+    0x03B5, 0x03B6,
+    0x04B0, 0x04E2,
+    0x04E3, 0x04E4,
+    0x04E5, 0x04E6,
+    0x04E7, 0x04E8
+}
+
 class PEObject(object):
     pe = None
 
@@ -174,6 +229,8 @@ class PEObject(object):
         except FileNotFoundError:
             print("ERROR: Could not find " + filepath, file=sys.stderr)
 
+    # Private, given a 32 bit word, converts word into a string of 2 16 bit integers
+    # seperated by '.' to conform to version string format.
     def __versionStr__(self, val):
         return str(((val & ~0) >> 16) & 0xffff) + "." + str(val & 0xffff)
 
@@ -220,7 +277,7 @@ class PEObject(object):
         try:
             vs_fixedfileinfoDict = self.pe.VS_FIXEDFILEINFO[0].dump_dict()
             for key in vs_fixedfileinfoDict.keys():
-                # Skip sections that have a dependencies
+                # Skip sections that have dependencies
                 if (key == PE_STRUCT_STR 
                   or key == FILE_SUBTYPE_STR 
                   or key == FILE_VERSION_LS_STR 
@@ -279,20 +336,28 @@ class PEObject(object):
 
         return result
 
-    # Parses PE/PE+ loaded into PEObject and returns JSON contaning metadata from header and .rsrc section
-    # def getVersionJSON(self):
-    #     if not self.pe:
-    #         return
-
-    #     return json.dumps(self.getVersionDict())
-
-    # Validates header and .rsrc section of PE/PE+ loaded into PEObject. Returns true if all required
-    # fields are valid and present, false otherwise.
-    # def validate(self):
-    #     versionDict = self.getVersionDict()
-    #     if versionDict[SIGNATURE_STR] != VALID_SIGNATURE:
-    #         return False
-
+# Given a JSON file, object creates generator for VERSIONINFO rc file for the given JSON.
+# Provides validaiton of some basic requirements and generates VERSIONINFO.rc source to be used with resource compiler.
+# Here is an example of the minimum required JSON
+# {
+# 	"FileVersion": "1.0.0.0",
+# 	"ProductVersion": "1.0.0.0",
+# 	"FileFlagsMask": "VS_FFI_FILEFLAGSMASK",
+# 	"FileFlags": "0",
+# 	"FileOS": "VOS_NT",
+# 	"FileType": "VFT_DRV",
+# 	"FileSubtype": "VFT2_DRV_SYSTEM",
+# 	"StringFileInfo": {
+# 		"CompanyName": "Example Company",
+# 		"ProductName": "Example Product",
+# 		"FileVersion": "1.0.0.0",
+# 	},
+# 	"VarFileInfo": {
+# 		"Translation": "0x0409 0x04b0"
+# 	}
+# }
+#
+# StringFileInfo and VarFileInfo can both have many more entries. More information available at https://docs.microsoft.com/en-us/windows/win32/menurc/versioninfo-resource.
 class VERSIONINFOGenerator(object):
     versionDict = None
 
@@ -300,8 +365,11 @@ class VERSIONINFOGenerator(object):
         try:
             with open(filepath, "r") as jsonFile:
                 data = jsonFile.read()
-                print(data)
-                self.versionDict = json.loads(data)
+                try:
+                    self.versionDict = json.loads(data)
+                except json.decoder.JSONDecodeError as e:
+                    print(e, file=sys.stderr)
+
         except FileNotFoundError:
             print("ERROR: Could not find " + filepath, file=sys.stderr)
 
@@ -390,13 +458,65 @@ class VERSIONINFOGenerator(object):
         else:
             valid = False
 
+        if TRANSLATION_STR in self.versionDict[VAR_FILE_INFO_STR]:
+            langIDset = self.versionDict[VAR_FILE_INFO_STR][TRANSLATION_STR].split(" ")
+            if len(langIDset) != 2:
+                print("ERROR: Translation field must contain 2 space delimited hexidecimal 8 bit words." , file=sys.stderr)
+                valid = False
+            elif langIDset[0] not in VALID_LANG_ID:
+                print("ERROR: Invalid language code: " + str(langIDset[0]) + ".", file=sys.stderr)
+                valid = False
+            elif langIDset[1] not in VALID_CHARSET_ID:
+                print("ERROR: Invalid charset code: " + str(langIDset[1]) + ".", file=sys.stderr)
+                valid = False
+        else:
+            print("ERROR: Missing required parameter: Translation in VarFileInfo" , file=sys.stderr)
+            valid = False
+
         return valid
+    
+    def write(self, path):
+        if not self.validate():
+            return
+
+        outStr = "/* Auto-generated VERSIONINFO resource file. */\n\n"
+        outStr += "#include <ntdef.h>\n#include <winver.h>\n#ifdef RC_INVOKED\n"
+
+        # Header fields
+        outStr += "VS_VERSION_INFO\tVERSIONINFO\n"
+        for param in self.versionDict.keys():
+            if param == STRING_FILE_INFO_STR or param == VAR_FILE_INFO_STR or param not in VERSIONFILE_REQUIRED_FIELDS:
+                continue
+            outStr += param + "\t" + self.versionDict[param] + "\n"
+
+        # StringFileInfo
+        outStr += "\n" + BEGIN_STR + "\n\t" + BLOCK_STR + " \"" + STRING_FILE_INFO_STR + "\"\n\t" + BEGIN_STR + "\n"
+        
+        languageCode = ""
+        for code in self.versionDict[VAR_FILE_INFO_STR][TRANSLATION_STR].split(" "):
+            languageCode += code.split("0x", 1)[0]
+
+        outStr += "\t\t" + BLOCK_STR + " \"" + languageCode + "\"\n\t\t" + BEGIN_STR + "\n"
+        for field in self.versionDict[STRING_FILE_INFO_STR].keys():
+            outStr += "\t\t" + VALUE_STR + " \"" + field + "\",\t\"" + self.versionDict[STRING_FILE_INFO_STR][field] + "\"\n"
+
+        outStr += "\t\t" + END_STR + "\n\t" + END_STR + "\n\n"
+    
+        # VarFileInfo
+        languageTokens = self.versionDict[VAR_FILE_INFO_STR][TRANSLATION_STR].split(" ")
+        for field in self.versionDict[VAR_FILE_INFO_STR].keys():
+            if field == TRANSLATION_STR:
+                outStr += "\t\t" + VALUE_STR + " \"" + field + "\",\t" + languageTokens[0] + "," + languageTokens[1] + "\n"
+            else:
+                outStr += "\t\t" + VALUE_STR + " \"" + field + "\",\t\"" + self.versionDict[VAR_FILE_INFO_STR][field] + "\"\n"
+
+        outStr += "\t" + END_STR + "\n" + END_STR + "\n#endif"
+
+        with open(path + "VERSIONINFO.rc", "w") as out:
+            out.write(outStr)
 
 
-
-# obj = PEObject(".\\tests\\test1\\test1rsrc.exe")
-# f = open("test1.json", "w")
-# json.dump(obj.getVersionDict(), f)
-# f.close()
+obj = PEObject(".\\tests\\test1\\test1rsrc.exe")
+print(json.dumps(obj.getVersionDict()))
 generator = VERSIONINFOGenerator(".\\test1.json")
-print(generator.validate())
+generator.write(".\\tests\\test1\\")
