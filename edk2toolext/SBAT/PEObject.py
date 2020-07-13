653ee4489665a4b5962d305fae68f00bc7f7b54f
Garand Tyson <t-gatys@microsoft.com>
2020-07-13 15:28:40 -0500

    Added validation for JSON VERSIONINFO inputs

--- edk2toolext/SBAT/PEObject.py
+++ edk2toolext/SBAT/PEObject.py
@@ -64,7 +64,7 @@ FILE_VERSION_STR         = "FileVersion"
 PRODUCT_VERSION_MS_STR   = "ProductVersionMS"
 PRODUCT_VERSION_LS_STR   = "ProductVersionLS"
 PRODUCT_VERSION_STR      = "ProductVersion"
-FILE_FLAG_MASK_STR       = "FileFlagMask"
+FILE_FLAG_MASK_STR       = "FileFlagsMask"
 FILE_FLAGS_STR           = "FileFlags"
 FILE_TYPE_STR            = "FileType"
 FILE_SUBTYPE_STR         = "FileSubtype"
@@ -82,12 +82,97 @@ PE_ENCODING              = "utf-8"
 PE_STRUCT_STR            = "Structure"
 PE_VALUE_STR             = "Value"
 
+# Validation requirements
+VERSIONFILE_REQUIRED_FIELDS = {
+    FILE_VERSION_STR,
+    PRODUCT_VERSION_STR,
+    FILE_FLAG_MASK_STR,
+    FILE_FLAGS_STR,
+    FILE_OS_STR,
+    FILE_TYPE_STR,
+    FILE_SUBTYPE_STR,
+    STRING_FILE_INFO_STR,
+    VAR_FILE_INFO_STR
+}
+
+VERSIONFILE_ALLOWED_FIELDS = {
+    FILE_VERSION_STR,
+    PRODUCT_VERSION_STR,
+    FILE_FLAG_MASK_STR,
+    FILE_FLAGS_STR,
+    FILE_OS_STR,
+    FILE_TYPE_STR,
+    FILE_SUBTYPE_STR,
+    STRING_FILE_INFO_STR,
+    VAR_FILE_INFO_STR,
+    SIGNATURE_STR,
+    STRUC_VERSION_STR,
+    FILE_DATE_STR,
+    FILE_DATE_MS_STR,
+    FILE_DATE_LS_STR
+}
+
+COMPANY_NAME_STR = "CompanyName"
+PRODUCT_NAME_STR = "ProductName"
+
+STRING_FILE_INFO_REQUIRED_FIELDS = {
+    COMPANY_NAME_STR,
+    PRODUCT_NAME_STR,
+    FILE_VERSION_STR
+}
+
+VALID_FILE_OS_VALUES = {
+    "VOS_UNKNOWN",
+    "VOS_DOS",
+    "VOS_NT",
+    "VOS__WINDOWS16",
+    "VOS__WINDOWS32",
+    "VOS_DOS_WINDOWS16",
+    "VOS_DOS_WINDOWS32",
+    "VOS_NT_WINDOWS32"
+}
+
+VALID_FILE_TYPE_VALUES = {
+    "VFT_APP",
+    "VFT_DLL",
+    "VFT_DRV",
+    "VFT_FONT",
+    "VFT_STATIC_LIB",
+    "VFT_UNKNOWN",
+    "VFT_VXD"
+}
+
+VALID_SUBTYPE_VFT_DRV = {
+    "VFT2_UNKNOWN",
+    "VFT2_DRV_COMM",
+    "VFT2_DRV_PRINTER",
+    "VFT2_DRV_KEYBOARD",
+    "VFT2_DRV_LANGUAGE",
+    "VFT2_DRV_DISPLAY",
+    "VFT2_DRV_MOUSE",
+    "VFT2_DRV_NETWORK",
+    "VFT2_DRV_SYSTEM",
+    "VFT2_DRV_INSTALLABLE",
+    "VFT2_DRV_SOUND",
+    "VFT2_DRV_VERSIONED_PRINTER"
+}
+
+VALID_SUBTYPE_VFT_FONT = {
+    "VFT2_UNKNOWN",
+    "VFT2_FONT_RASTER",
+    "VFT2_FONT_VECTOR",
+    "VFT2_FONT_TRUETYPE"
+}
+
 class PEObject(object):
     pe = None
 
     # Takes filepath to PE/PE+ file to parse
     def __init__(self, filepath):
-        self.pe = pefile.PE(filepath)
+        try:
+            self.pe = pefile.PE(filepath)
+        except FileNotFoundError:
+            print("ERROR: Could not find " + filepath, file=sys.stderr)
 
     def __versionStr__(self, val):
         return str(((val & ~0) >> 16) & 0xffff) + "." + str(val & 0xffff)
@@ -118,6 +203,9 @@ class PEObject(object):
 
     # Returns true if PE object contains .rsrc section, false otherwise
     def containsrsrc(self):
+        if not self.pe:
+            return
+
         for section in self.pe.sections:
             if section.Name.decode(PE_ENCODING).replace("\x00", "") == RSRC_STR:
                 return True
@@ -125,6 +213,9 @@ class PEObject(object):
     
     # Parses PE/PE+ loaded into PEObject and returns dictionary contaning metadata from header and .rsrc section
     def getVersionDict(self):
+        if not self.pe:
+            return
+
         result = {}
         try:
             vs_fixedfileinfoDict = self.pe.VS_FIXEDFILEINFO[0].dump_dict()
@@ -189,15 +280,123 @@ class PEObject(object):
         return result
 
     # Parses PE/PE+ loaded into PEObject and returns JSON contaning metadata from header and .rsrc section
-    def getVersionJSON(self):
-        return json.dumps(self.getVersionDict())
+    # def getVersionJSON(self):
+    #     if not self.pe:
+    #         return
+
+    #     return json.dumps(self.getVersionDict())
 
     # Validates header and .rsrc section of PE/PE+ loaded into PEObject. Returns true if all required
     # fields are valid and present, false otherwise.
+    # def validate(self):
+    #     versionDict = self.getVersionDict()
+    #     if versionDict[SIGNATURE_STR] != VALID_SIGNATURE:
+    #         return False
+
+class VERSIONINFOGenerator(object):
+    versionDict = None
+
+    def __init__(self, filepath):
+        try:
+            with open(filepath, "r") as jsonFile:
+                data = jsonFile.read()
+                print(data)
+                self.versionDict = json.loads(data)
+        except FileNotFoundError:
+            print("ERROR: Could not find " + filepath, file=sys.stderr)
+
+    # Private, returns true if verStr is a valid version string, false otherwise.
+    def __validateVersionNumber__(self, verStr):
+        if verStr.count('.') != 3:
+            print("ERROR: Invalid version string: " + verStr + ". Version must be in form \" \
+                   INTEGER.INTEGER.INTEGER.INTEGER\".", file=sys.stderr)
+            return False
+        
+        for substr in verStr.split("."):
+            try:
+                if int(substr) > 65535:
+                    print("WARNING: Integer overflow in version string: " + verStr + ".", file=sys.stderr)
+            except ValueError:
+                print("ERROR: Invalid version string: " + verStr + ". Version must be in form \" \
+                       INTEGER.INTEGER.INTEGER.INTEGER\".", file=sys.stderr)
+                return False
+        
+        return True
+
+    # Checks if loaded JSON file represents a valid VERSIONINFO resource. Returns true if JSON
+    # is valid, print relevant error messages and returns false otherwise.
     def validate(self):
-        versionDict = self.getVersionDict()
-        if versionDict[SIGNATURE_STR] != VALID_SIGNATURE:
+        if not self.versionDict:
             return False
 
-obj = PEObject(".\\tests\\test1\\test1norsrc.exe")
-print(obj.getVersionJSON())
+        valid = True
+
+        # First Pass: Check to see if all required fields are present
+        required = {string.upper() for string in VERSIONFILE_REQUIRED_FIELDS}
+        allowed = {string.upper() for string in VERSIONFILE_ALLOWED_FIELDS}
+        for key in self.versionDict.keys():
+            if key.upper() not in allowed:
+                print("ERROR: Invalid parameter: " + key + ".", file=sys.stderr)
+                valid = False
+            else:
+                if key.upper() in required:
+                    required.remove(key.upper())
+        
+        for remaining in required:
+            print("ERROR: Missing required parameter: " + remaining + ".", file=sys.stderr)
+            valid = False
+
+        if STRING_FILE_INFO_STR in self.versionDict:
+            requiredStringFileFields = STRING_FILE_INFO_REQUIRED_FIELDS
+            for key in self.versionDict[STRING_FILE_INFO_STR]:
+                if key in requiredStringFileFields:
+                    requiredStringFileFields.remove(key)
+            
+            for remaining in requiredStringFileFields:
+                print("ERROR: Missing required StringFileInfo parameter: " + remaining + ".", file=sys.stderr)
+                valid = False
+        
+        if not valid:
+            return False
+        
+        # Second pass: Check to see if fields are valid
+        valid = self.__validateVersionNumber__(self.versionDict[FILE_VERSION_STR]) \
+                and self.__validateVersionNumber__(self.versionDict[PRODUCT_VERSION_STR])
+
+        if self.versionDict[FILE_OS_STR] not in VALID_FILE_OS_VALUES:
+            print("ERROR: Invalid FILEOS value: " + self.versionDict[FILE_OS_STR] + ".", file=sys.stderr)
+            valid = False
+        
+        if self.versionDict[FILE_TYPE_STR] not in VALID_FILE_TYPE_VALUES:
+            print("ERROR: Invalid FILETYPE value: " + self.versionDict[FILE_TYPE_STR] + ".", file=sys.stderr)
+            valid = False
+
+        if self.versionDict[FILE_TYPE_STR] == "VFT_DRV":
+            if self.versionDict[FILE_SUBTYPE_STR] not in VALID_SUBTYPE_VFT_DRV:
+                print("ERROR: Invalid FILESUBTYPE value for FILETYPE VFT_DRV: " + self.versionDict[FILE_SUBTYPE_STR] + ".", file=sys.stderr)
+                valid = False
+        elif self.versionDict[FILE_TYPE_STR] == "VFT_FONT":
+            if self.versionDict[FILE_SUBTYPE_STR] not in VALID_SUBTYPE_VFT_FONT:
+                print("ERROR: Invalid FILESUBTYPE value for FILETYPE VFT_FONT: " + self.versionDict[FILE_SUBTYPE_STR] + ".", file=sys.stderr)
+                valid = False
+        elif self.versionDict[FILE_TYPE_STR] != "VFT_VXD" and self.versionDict[FILE_SUBTYPE_STR] != 0:
+            print("ERROR: Invalid FILESUBTYPE value for FILETYPE " + self.versionDict[FILE_TYPE_STR] + ", value must be 0.", file=sys.stderr)
+            valid = False
+
+        if self.__validateVersionNumber__(self.versionDict[STRING_FILE_INFO_STR][FILE_VERSION_STR]):
+            if self.versionDict[STRING_FILE_INFO_STR][FILE_VERSION_STR] != self.versionDict[FILE_VERSION_STR]:
+                print("ERROR: FILEVERSION in header does not match FileVersion in StringFileInfo." , file=sys.stderr)
+                valid = False
+        else:
+            valid = False
+
+        return valid
+
+
+
+# obj = PEObject(".\\tests\\test1\\test1rsrc.exe")
+# f = open("test1.json", "w")
+# json.dump(obj.getVersionDict(), f)
+# f.close()
+generator = VERSIONINFOGenerator(".\\test1.json")
+print(generator.validate())
