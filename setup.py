c91885293cc516af974a30fb7c6511448e6eb898
Jeremiah Cox <17728431+out0xb2@users.noreply.github.com>
2020-09-17 20:30:49 -0700

    Adding firmware_policy_tool to setup.py

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setuptools.setup(
                             'omnicache=edk2toolext.omnicache:main',
                             'nuget-publish=edk2toolext.nuget_publishing:go',
                             'sig_db=edk2toolext.uefi.sig_db:main',
+                            'firmware_policy_tool=edk2toolext.windows.policy.firmware_policy_tool:main',
                             'edk2_capsule_tool=edk2toolext.capsule.capsule_tool:main']
     },
     install_requires=[
