23fbd241f9538e87263d022bfa0db8242cf37bb8
brbarkel@microsoft.com <brbarkel@microsoft.com>
2021-06-10 15:04:45 -0700

    Bump the library version to get the InfGenerator stuff

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setuptools.setup(
     },
     install_requires=[
         'pyyaml>=5.3.1',
-        'edk2-pytool-library>=0.10.13',
+        'edk2-pytool-library>=0.10.14',
         'pefile>=2019.4.18'
     ],
     extras_require={
